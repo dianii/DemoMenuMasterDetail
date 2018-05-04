1bf91d2e25cc67eb0b94ea4262c9b417256a952d
dianii <38058753+dianii@users.noreply.github.com>
2018-05-03 20:58:17 -0500

    MenuMasterDetail

---
+++ DemoMenuMasterDetail/DemoMenuMasterDetail/DemoMenuMasterDetail/MasterPageItem.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoMenuMasterDetail
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    class MasterPageItem
+    {
+        public string Titulo { get; set; }
+
+        public string Icono { get; set; }
+
+        public Type TipoPagina { get; set; }
+    }
+}
